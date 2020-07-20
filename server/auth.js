32439605c7d75254a533150e4667220954347012
Vrdo12 <stepanyan-vardan@list.ru>
2020-07-21 00:05:34 +0400

    parser

--- server/auth.js
+++ server/auth.js
@@ -1,9 +1,11 @@
 const { json } = require('express');
 const db = require('./db');
+const bodyParser = require('body-parser')
 const auth = require('express').Router();
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
-auth.post('/log', (req, res) =>{
+auth.post('/log', urlencodedParser, (req, res) =>{
     const data = {
         login: req.body.login,
         password: req.body.password
